e1dfccb095c7796491eaf770cac5454951e63563
Kylemerian <s02190176@gse.cs.msu.ru>
2022-09-02 01:45:37 +0300

    connected db & added some methods for Database

--- client.cpp
+++ client.cpp
@@ -1,7 +1,9 @@
 #include <SFML/Network.hpp>
 #include <string>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
+using namespace std;
 
 int main(){
     sf::TcpSocket socket;
@@ -9,11 +11,37 @@ int main(){
     if (status != sf::Socket::Done){
         std::cout << "Didn't connect to server\n";
     }
-    std::string s = "Hello!\n";
+    std::string s = "#Auth log pass\n";
     sf::Packet packet;
     packet << s;
     if (socket.send(packet) != sf::Socket::Done){
         std::cout << "Didn't send\n";
     }
+
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "Chat");
+    sf::Image image;
+    image.loadFromFile("bg.png");
+    sf::Texture bgTex;
+    bgTex.loadFromImage(image);
+    sf::Sprite bgSprite;
+    bgSprite.setTexture(bgTex);
+    sf::Vector2f targetSize(1024.0f, 768.0f);
+    bgSprite.setScale(targetSize.x / bgSprite.getLocalBounds().width, targetSize.y / bgSprite.getLocalBounds().height);
+
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed){
+                window.close();
+                socket.disconnect();
+            }
+        }
+       
+        window.draw(bgSprite);
+        window.display();
+        window.clear();
+    }
+
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -2,13 +2,98 @@
 #include <SFML/Network.hpp>
 #include <vector>
 #include <string>
+#include <pqxx/pqxx>
 
-void printClients(std::vector <sf::TcpSocket*>& clients){
-    for(auto it : clients)
-        std::cout << it->getRemoteAddress() << " " << it->getRemotePort() << "\n";
+enum{
+    authCode,
+    regCode,
+    messageCode,
+    historyCode,
+    errCode = -1
+};
+
+class Database{
+public:
+    pqxx::connection conn;
+    pqxx::work w;
+    Database():w(conn), conn("user=postgres password=1 host=localhost port=5432 dbname=postgres"){
+        /*pqxx::result r = w.exec("SELECT * from clients where client_id = 1;");
+        w.commit();
+        std::cout << r[0][0].c_str() << r[0][1].c_str() << r[0][2].c_str() << r[0][3].c_str() << std::endl;*/
+        //  message(2, 3, "mess", "\'2010-10-10 00:00:00\'");
+        //regClient("nick2", "log2", "pass2");
+        std::cout << authClient("log2", "pass") << " = id\n";
+    }
+
+    int authClient(std::string log, std::string pass){
+        pqxx::result r = w.exec("SELECT * FROM clients WHERE password = \'" + pass + "\' AND login = \'" + log + "\';");
+        w.commit();
+        if(r.empty())
+            return -1;
+        else
+            return atoi(r[0][0].c_str());
+    }
+
+    int regClient(std::string nickname, std::string log, std::string pass){
+        pqxx::result r = w.exec("SELECT * FROM clients WHERE login = \'" + log + "\';");
+        if(r.size() != 0)
+            return -1;
+        r = w.exec("INSERT INTO clients(nickname, login, password)"
+                                "VALUES(\'"+ nickname + "\', \'" + log + "\', \'" + pass +"\');");
+        w.commit();
+        return 0;
+    }
+
+    int message(int sender_id, int receiver_id, std::string message, std::string time){
+        pqxx::result r = w.exec("INSERT INTO messages(sender_id, receiver_id, time, message)"
+                                "VALUES(" + std::to_string(sender_id) + ", " + std::to_string(receiver_id) + ", " + time + ", \'" + message + "\');");
+        w.commit();
+        return r.size();
+    }
+
+    int getHistoryMessages(int client_id){
+        //get req to db
+        return 1;
+    }
+    ~Database(){
+        conn.close();
+    }
+};
+
+
+
+
+int requestIdentify(std::string request){
+    std::vector <std::string> requests = {"#auth", "#message", "#register"};
+    for(int i = 0; i < requests.size(); i++){
+        if(request.substr(0, requests[i].size()) == request)
+            return i;
+    }
+    return -1;
+}
+
+void requestHandler(int reqCode, Database &db){
+    switch (reqCode)
+    {
+    case authCode:
+        //db.authClient();
+        break;
+    case regCode:
+        //db.regClient();
+        break;
+    case messageCode:
+        //db.message();
+        break;
+    case historyCode:
+        //db.getHistoryMessages();
+        break;
+    case errCode:
+        return;
+    }
 }
 
 int main(){
+    Database db;
     // Create a socket to listen to new connections
     sf::TcpListener listener;
     listener.listen(55000);
@@ -34,7 +119,7 @@ int main(){
                     // Add the new client to the selector so that we will
                     // be notified when he sends something
                     selector.add(*client);
-                    std::cout << "new client\n";
+                    std::cout << "new connection\n";
                     //printClients(clients);/**/
                 }
                 else{
@@ -49,10 +134,12 @@ int main(){
                     if (selector.isReady(client)){
                         // The client has sent some data, we can receive it
                         sf::Packet packet;
-                        std::string t;
+                        std::string request;
                         if (client.receive(packet) == sf::Socket::Done){
-                            packet >> t;
-                            std::cout << t << "\n";
+                            packet >> request;
+                            std::cout << request << "\n";
+                            int reqCode = requestIdentify(request);
+                            requestHandler(reqCode);
                         }
                     }
                 }
